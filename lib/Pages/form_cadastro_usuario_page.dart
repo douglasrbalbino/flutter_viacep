bbb172d2259a4518021d8d138c7cabcc58ee843f
Douglas <doug.balbino@hotmail.com>
2025-10-21 22:05:21 -0300

    adicao do form deusuario

---
+++ lib/Pages/form_cadastro_usuario_page.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class FormCadastroUsuarioPage extends StatefulWidget {
+  const FormCadastroUsuarioPage({super.key});
+
+  @override
+  State<FormCadastroUsuarioPage> createState() =>
+      _FormCadastroUsuarioPageState();
+}
+
+class _FormCadastroUsuarioPageState extends State<FormCadastroUsuarioPage> {
+  TextEditingController nomeController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController telefoneController = TextEditingController();
+  TextEditingController cpfController = TextEditingController();
+  TextEditingController senhaController = TextEditingController();
+  TextEditingController confirmacaoController = TextEditingController();
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          child: Column(
+            children: [
+              Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  spacing: 20,
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Insira seu nome";
+                        }
+                        return null;
+                      },
+                      controller: nomeController,
+                      decoration: InputDecoration(
+                        labelText: "Nome",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "O campo email é obrigatório";
+                        }
+
+                        if (!value.contains("@")) {
+                          return "Email invalido";
+                        }
+                        return null;
+                      },
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "O campo telefone é obrigatório";
+                        }
+                        return null;
+                      },
+                      controller: telefoneController,
+                      decoration: InputDecoration(
+                        labelText: "Telefone",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    TextFormField(
+                      maxLength: 11,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "O campo cpf é obrigatório";
+                        }
+                        return null;
+                      },
+                      controller: cpfController,
+                      decoration: InputDecoration(
+                        labelText: "CPF",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "O campo senha é obrigatório";
+                        }
+                        return null;
+                      },
+                      controller: senhaController,
+                      decoration: InputDecoration(
+                        labelText: "Senha",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Insira seu nome";
+                        }
+
+                        if (value != senhaController.text) {
+                          return "A confirmação da senha está incorreta";
+                        }
+                        return null;
+                      },
+                      controller: confirmacaoController,
+                      decoration: InputDecoration(
+                        labelText: "Confirme a senha",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        if (!formKey.currentState!.validate()) {
+                          return;
+                        }
+                        print("Formulário validado");
+                      },
+                      child: Text("Cadastrar"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_3/Pages/form_cadastro_usuario_page.dart';
 import 'package:flutter_application_3/Pages/home_page.dart';
 
 class ViaCepApi extends StatelessWidget {
@@ -10,9 +11,9 @@ class ViaCepApi extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      home: const FormCadastroUsuarioPage(),
     );
   }
 }
